0b91a688f1c16f13a7cae142dc4d7b0b7be468dd
Shivatimika <pawarharsha221@gmail.com>
2025-12-15 01:14:11 +0530

    Add new routes for reels

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Signup from "./pages/Signup.jsx";
 import Feed from "./pages/Feed.jsx";
 import Profile from "./pages/Profile.jsx";
 import Navbar from "./components/Navbar.jsx";
+import Home from "./components/home.jsx";
+import Feedd from "./pages/Feed.jsx";
+import SearchUsers from "./pages/SearchUsers.jsx";
+import Reels from "./pages/Reels.jsx";
 
 function App() {
   return (
@@ -14,11 +18,17 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/SearchUsers" element={<SearchUsers />} />
+        <Route path="/Feed" element={<Feedd />} />
+        <Route path="/Reels" element={<Reels />} />
       </Routes>
     </>
   );
 }
 
 export default App;
+
+
 
 
